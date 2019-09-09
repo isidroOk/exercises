13dceb254426eb072fcf8e6f866810690c354cae
Marcos Bellucci <mbellucci@salesforce.com>
2019-09-09 13:04:18 -0300

    Add testing

--- random_number.rb
+++ random_number.rb
@@ -1,12 +1,54 @@
-puts "Enter a number you want:"
+require 'byebug'
 
-number = gets.chomp
-number = number.to_i
+# HOW
+def game(number)
+  r_number = rand(number) + 1
 
-r_number = rand(number) + 1
+  if r_number > 10
+    "YOU WON"
+  else
+    "YOU LOST"
+  end
+end
 
-if r_number > 10
-    print "YOU WON"
-else
-    print "YOU LOST"
+
+def user_interaction
+  puts "Enter a number you want:"
+
+  number = gets.chomp
+  number = number.to_i
+
+  print game(number)
+end
+
+
+# WHAT
+describe '#game' do
+  context 'when rand give us a number more than 10 and input is 1' do
+    let(:input) { 1 }
+    before do
+      # Arrange
+      allow(self).to receive(:rand).and_return(10)
+    end
+
+    it 'returns YOU WON' do
+      result = game(input) # Act
+
+      expect(result).to eq('YOU WON') # Assert
+    end
+  end
+
+  context 'when rand give us a number less than 10 and input is 1' do
+    let(:input) { 1 }
+    before do
+      # Arrange
+      allow(self).to receive(:rand).and_return(8)
+    end
+
+    it 'returns YOU LOST' do
+      result = game(input) # Act
+
+      expect(result).to eq('YOU LOST') # Assert
+    end
+  end
 end
